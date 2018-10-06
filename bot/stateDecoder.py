49122003788b906cb64f926f73a74062568b7bb7
Vincent Gagnon <vg_4_@hotmail.fr>
2018-10-06 13:27:09 -0400

    changements pour guillaume

--- bot/stateDecoder.py
+++ bot/stateDecoder.py
@@ -1,13 +1,28 @@
 import numpy as np
 
-class stateDecoder:
-
-def stateDecoder(gameMap, visiblePlayers):
+def stateEncoder(gameMap, visiblePlayers):
     vecteur = []
     for i in range(len(gameMap.tiles)-1):
         vecteur.append(gameMap.tiles[i])
 
-    for j in range(len(visiblePlayers)-1):
-        vecteur.append(visiblePlayers[j])
+    for m in range(0,11,4 ):
+        vecteur.append(0)
+        vecteur.append(0)
+        vecteur.append(0)
+        vecteur.append(0)
+    if(len(visiblePlayers)>2):
+        for j in range(0,11,4 ):
+        vecteur.append(visiblePlayers[j].Health)
+        vecteur.append(visiblePlayers[j+1].Position)
+        vecteur.append(visiblePlayers[j+2].TotalResources)
+        vecteur.append(visiblePlayers[j+3].AttackPower)
+    elif (len(visiblePlayers) == 0):
 
-    return vecteur
+    else:
+        for k in range(0,(len(visiblePlayer)*4)-1,4):
+            vecteur.append(visiblePlayers[k].Health)
+            vecteur.append(visiblePlayers[k+1].Position)
+            vecteur.append(visiblePlayers[k+2].TotalResources)
+            vecteur.append(visiblePlayers[k+3].AttackPower)
+
+return vecteur
