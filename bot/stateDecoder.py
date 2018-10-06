5ff2e6582965dd077844fd01490649f624ea8bce
Vincent Gagnon <vg_4_@hotmail.fr>
2018-10-06 13:58:22 -0400

    modif de statedec

--- bot/stateDecoder.py
+++ bot/stateDecoder.py
@@ -1,28 +1,37 @@
 import numpy as np
 
-def stateEncoder(gameMap, visiblePlayers):
+def stateDecoder(gameMap, visiblePlayers):
     vecteur = []
     for i in range(len(gameMap.tiles)-1):
         vecteur.append(gameMap.tiles[i])
 
-    for m in range(0,11,4 ):
-        vecteur.append(0)
-        vecteur.append(0)
-        vecteur.append(0)
-        vecteur.append(0)
-    if(len(visiblePlayers)>2):
-        for j in range(0,11,4 ):
-        vecteur.append(visiblePlayers[j].Health)
-        vecteur.append(visiblePlayers[j+1].Position)
-        vecteur.append(visiblePlayers[j+2].TotalResources)
-        vecteur.append(visiblePlayers[j+3].AttackPower)
-    elif (len(visiblePlayers) == 0):
 
-    else:
-        for k in range(0,(len(visiblePlayer)*4)-1,4):
+    if(len(visiblePlayers)>2):
+        for j in range(3):
+            vecteur.append(visiblePlayers[j].Health)
+            vecteur.append(visiblePlayers[j].Position)
+            vecteur.append(visiblePlayers[j].TotalResources)
+            vecteur.append(visiblePlayers[j].AttackPower)
+    elif (len(visiblePlayers) == 0):
+        for l in range(12):
+            vecteur.append(0)
+    elif((len(visiblePlayer) == 1 ):
+        for k in range(1):
             vecteur.append(visiblePlayers[k].Health)
-            vecteur.append(visiblePlayers[k+1].Position)
-            vecteur.append(visiblePlayers[k+2].TotalResources)
-            vecteur.append(visiblePlayers[k+3].AttackPower)
+            vecteur.append(visiblePlayers[k].Position)
+            vecteur.append(visiblePlayers[k].TotalResources)
+            vecteur.append(visiblePlayers[k].AttackPower)
+        for n in range(8):
+            vecteur.append(0)
+
+    elif((len(visiblePlayer) == 1 ):
+        for k in range(2):
+            vecteur.append(visiblePlayers[k].Health)
+            vecteur.append(visiblePlayers[k].Position)
+            vecteur.append(visiblePlayers[k].TotalResources)
+            vecteur.append(visiblePlayers[k].AttackPower)
+        for n in range(4):
+            vecteur.append(0)
+
 
 return vecteur
